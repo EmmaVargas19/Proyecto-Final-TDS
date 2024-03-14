7a43bdff34ee81b2cfc0d4ba4c5b1e37d42e194f
Eudy Familia Pérez <eudyfperez@gmail.com>
2024-03-14 13:09:18 -0400

    cambios en eventos terminados y perfil agregado
    cambios en eventos terminados y perfil agregado

--- vite-project/src/Eventos.jsx
+++ vite-project/src/Eventos.jsx
@@ -6,22 +6,23 @@ import 'react-toastify/dist/ReactToastify.css';
 import './App.css'
 
 export function Eventos() {
-    const { user, logged, toastifye, localStorageGetEdit, localStorageGetBorrar } = useContext(contextName);
-
     const [eventosInscritos, setEventosInscritos] = useState([]);
+    const { user, logged, toastifye, localStorageGet, localStorageGetEdit, localStorageGetBorrar } = useContext(contextName);
 console.log(eventosInscritos)
+console.log("soy eventos inscritos")
+console.log(localStorageGet() + "soy local storage get")
     const events = [
-        { name: "Evento 1", date: "2021-10-10", description: "Este es el evento 1", img: "src/assets/img1.jpeg", id: 1 },
-        { name: "Evento 2", date: "2021-10-11", description: "Este es el evento 2", img: "src/assets/img2.jpeg", id: 2 },
-        { name: "Evento 3", date: "2021-10-12", description: "Este es el evento 3", img: "src/assets/img3.jpeg", id: 3 },
-        { name: "Evento 4", date: "2021-10-13", description: "Este es el evento 4", img: "src/assets/img4.jpeg", id: 4 },
-        { name: "Evento 5", date: "2021-10-14", description: "Este es el evento 5", img: "src/assets/img5.jpeg", id: 5 }
+        { name: "Evento 1", date: "2021-10-10", description: "Este es el evento 1", img: "src/assets/img1.jpeg", id: crypto.randomUUID() },
+        { name: "Evento 2", date: "2021-10-11", description: "Este es el evento 2", img: "src/assets/img2.jpeg", id: crypto.randomUUID() },
+        { name: "Evento 3", date: "2021-10-12", description: "Este es el evento 3", img: "src/assets/img3.jpeg", id: crypto.randomUUID() },
+        { name: "Evento 4", date: "2021-10-13", description: "Este es el evento 4", img: "src/assets/img4.jpeg", id: crypto.randomUUID() },
+        { name: "Evento 5", date: "2021-10-14", description: "Este es el evento 5", img: "src/assets/img5.jpeg", id: crypto.randomUUID() }
     ];
 
     const handleInscribirse = (obj) => {
         if (logged) {
-            localStorageGetEdit(obj);
             setEventosInscritos([...eventosInscritos, obj]); // Agregar evento inscrito al estado local
+            localStorageGetEdit(obj);
         } else {
             toastifye("No puedes inscribirte si no estás registrado");
         }
@@ -31,7 +32,7 @@ console.log(eventosInscritos)
         if(logged){
             const filtrado = eventosInscritos.filter(obj => obj.id !== objj.id)
             setEventosInscritos(filtrado)
-            localStorageGetBorrar(eventosInscritos)
+            localStorageGetBorrar(filtrado)
         }
     }
 
@@ -63,7 +64,7 @@ console.log(eventosInscritos)
             </div>
             <h2>Eventos donde estoy inscrito</h2>
             <div className='gridejem'>
-                {user && logged ? mapeoEventosInscritos : <NoEventos />}
+                {user && logged && JSON.parse(localStorageGet()).inscrito.length !== 0 ? mapeoEventosInscritos : <NoEventos />}
             </div>
             <ToastContainer />
         </>
