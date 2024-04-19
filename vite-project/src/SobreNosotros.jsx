583a9598624b78a342a67350ca3e094ae29e8295
EmmaVargas19 <103398534+EmmaVargas19@users.noreply.github.com>
2024-04-19 09:42:20 -0400

    Update SobreNosotros.jsx

--- vite-project/src/SobreNosotros.jsx
+++ vite-project/src/SobreNosotros.jsx
@@ -28,7 +28,7 @@ useEffect(()=>{
     <section className='sobreNosotros'>
       <section className='ejemm hidden' id='mision'>
       <article className="articulo">
-        <h2>Mision</h2>
+        <h2>Misión</h2>
         <p>Nuestra misión en RenovaTech es promover la gestión responsable de los residuos electrónicos en la República Dominicana, fomentando la conciencia ambiental y facilitando la recolección, reciclaje y reutilización de dispositivos electrónicos obsoletos. Nos comprometemos a proteger el medio ambiente y la salud pública mediante la implementación de programas y proyectos sostenibles que reduzcan el impacto negativo de los residuos electrónicos en nuestra sociedad.</p>
       </article>
       <section className='ejemm-img'>
@@ -40,7 +40,7 @@ useEffect(()=>{
         <img src="/src/assets/vision.png" alt="" id='vision-img'/>
       </section>
       <article className="articulo">
-        <h2 id='vision'>Vision</h2>
+        <h2 id='vision'>Visión</h2>
         <p>Nuestra visión es convertirnos en líderes en el campo de la gestión de residuos electrónicos en la República Dominicana, siendo reconocidos por nuestra excelencia en la implementación de soluciones innovadoras y sostenibles. Nos esforzamos por crear un futuro en el que la gestión responsable de los residuos electrónicos sea una práctica estándar en nuestra sociedad, contribuyendo así a un entorno más limpio y saludable para las generaciones futuras.</p>
       </article>
       </section>
